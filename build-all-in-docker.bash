da42ed476ab5bda45b9da0e2d40549349c49b78e
sammtcbn <sammtcbn@gmail.com>
2023-12-01 18:41:33 +0800

    generate RoT Update Capsule

--- build-all-in-docker.bash
+++ build-all-in-docker.bash
@@ -1,5 +1,21 @@
 #!/bin/bash
-docker run --name intel-pfr-signing-utility --rm -it -v $(pwd):/home/intel/workspace --workdir="/home/intel/workspace" sammtcbn/intel-pfr-signing-utility ./pfr-image-create.bash
+function failed()
+{
+    echo "$*" >&2
+    exit 1
+}
+
+# PFR Image
+echo "generage PFR Image ..."
+docker run --name intel-pfr-signing-utility --rm -it -v $(pwd):/home/intel/workspace --workdir="/home/intel/workspace" sammtcbn/intel-pfr-signing-utility ./pfr-image-create.bash || failed "Failed to generate PFR Image "
+echo "done"
 
 # Decommission Capsule
-docker run --name intel-pfr-signing-utility --rm -it -v $(pwd)/rot:/home/intel/workspace/rot --workdir="/home/intel/workspace/rot" sammtcbn/intel-pfr-signing-utility ./rot_decommission_capsule_create.bash
+echo "generate Decommission Capsule ..."
+docker run --name intel-pfr-signing-utility --rm -it -v $(pwd)/rot:/home/intel/workspace/rot --workdir="/home/intel/workspace/rot" sammtcbn/intel-pfr-signing-utility ./rot_decommission_capsule_create.bash || failed "Failed to generate Decommission Capsule"
+echo "done"
+
+# RoT Update Capsule
+echo "generate RoT Update Capsule ..."
+docker run --name intel-pfr-signing-utility --rm -it -v $(pwd)/rot:/home/intel/workspace/rot --workdir="/home/intel/workspace/rot" sammtcbn/intel-pfr-signing-utility ./rot_update_capsule_create.bash || failed "Failed to generate Update Capsule"
+echo "done"
